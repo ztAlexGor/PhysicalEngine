771a7001b771874476eda1665decc2cf79844e39
ztAlexGor232 <ztalexgor@gmail.com>
2023-02-03 00:28:19 +0200

    Add getters and setters to class Body

--- PhysicalEngine/PhysicalEngine/Body.cpp
+++ PhysicalEngine/PhysicalEngine/Body.cpp
@@ -7,6 +7,7 @@ Body::Body(const BodyInit& init)
 	angle = init.angle;
 	angularVelocity = init.angularVelocity;
 	bIsStatic = init.bIsStatic;
+	gravityScale = init.gravityScale;
 	adittionalData = init.adittionalData;
 
 	for (Vector f : init.forces)
@@ -21,3 +22,108 @@ void Body::SetShape(Shape* newShape, MaterialInfo shapeMaterial, float density)
 	material = shapeMaterial;
 	massInfo = shape->ComputeMass(density);
 }
+
+const Shape* Body::GetShape() const
+{
+	return shape;
+}
+
+const MassInfo& Body::GetMassInfo() const
+{
+	return massInfo;
+}
+
+const MaterialInfo& Body::GetMaterial() const
+{
+	return material;
+}
+
+Vector Body::GetPosition() const
+{
+	return position;
+}
+
+Vector Body::GetVelocity() const
+{
+	return velocity;
+}
+
+float Body::GetAngle() const
+{
+	return angle;
+}
+
+float Body::GetAngularVelocity() const
+{
+	return angularVelocity;
+}
+
+bool Body::IsStatic() const
+{
+	return bIsStatic;
+}
+
+float Body::GetGravityScale() const
+{
+	return gravityScale;
+}
+
+const std::vector<Vector>& Body::GetForces() const
+{
+	return forces;
+}
+
+std::string Body::GetAdittionalData() const
+{
+	return adittionalData;
+}
+
+void Body::SetMaterial(MaterialInfo material)
+{
+	this->material = material;
+}
+
+void Body::SetPosition(Vector pos)
+{
+	position = pos;
+}
+
+void Body::SetVelocity(Vector vel)
+{
+	velocity = vel;
+}
+
+void Body::SetAngle(float newAngle)
+{
+	angle = newAngle;
+}
+
+void Body::SetAngularVelocity(float newAngularVelocity)
+{
+	angularVelocity = newAngularVelocity;
+}
+
+void Body::SetStatic(bool arg)
+{
+	bIsStatic = arg;
+}
+
+void Body::SetGravityScale(float newGravityScale)
+{
+	gravityScale = newGravityScale;
+}
+
+void Body::SetForces(std::vector<Vector>& forces)
+{
+	for (Vector f : forces)
+	{
+		this->forces.emplace_back(f);
+	}
+}
+
+void Body::SetAdittionalData(std::string data)
+{
+	adittionalData = data;
+}
+
+
--- PhysicalEngine/PhysicalEngine/Body.h
+++ PhysicalEngine/PhysicalEngine/Body.h
@@ -9,7 +9,6 @@ struct MaterialInfo
 	float restitution;
 	float staticFriction;
 	float dynamicFriction;
-	float gravityScale;
 };
 
 
@@ -20,13 +19,10 @@ struct BodyInit
 	float angle;
 	float angularVelocity;
 	bool bIsStatic;
+	float gravityScale;
 	std::vector<Vector> forces;
 	std::string adittionalData;
 };
-
-
-
-
 
 
 class Body
@@ -40,14 +36,55 @@ class Body
 	float angle;
 	float angularVelocity;
 	bool bIsStatic;
+	float gravityScale;
 	std::vector<Vector> forces;
 	std::string adittionalData;
 
 public:
 	Body(const BodyInit& init);
 	
+	// getters
+	const Shape* GetShape() const;
+
+	const MassInfo& GetMassInfo() const;
+
+	const MaterialInfo& GetMaterial() const;
+
+	Vector GetPosition() const;
+
+	Vector GetVelocity() const;
+
+	float GetAngle() const;
+
+	float GetAngularVelocity() const;
+
+	bool IsStatic() const;
+
+	float GetGravityScale() const;
+
+	const std::vector<Vector>& GetForces() const;
+
+	std::string GetAdittionalData() const;
+
+	// setters
 	void SetShape(Shape* newShape, MaterialInfo material, float density);
 
+	void SetMaterial(MaterialInfo material);
 
+	void SetPosition(Vector pos);
+
+	void SetVelocity(Vector vel);
+
+	void SetAngle(float newAngle);
+
+	void SetAngularVelocity(float newAngularVelocity);
+
+	void SetStatic(bool arg);
+
+	void SetGravityScale(float newGravityScale);
+
+	void SetForces(std::vector<Vector>& forces);
+
+	void SetAdittionalData(std::string data);
 };
 
